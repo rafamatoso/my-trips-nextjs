eb07284a76a1e9b259acebf74f9d79b59e140234
Rafael Matoso <rafaelmatosog@gmail.com>
2021-07-02 20:16:22 -0300

    Verificando site no Google Search Console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="U_ujlVIFaxK0OlRjGfWDFkExpDIZSzXl47NNsfKBcLk"
+        />
         <link
           rel="shortcut icon"
           href="/img/icon-512.png"
