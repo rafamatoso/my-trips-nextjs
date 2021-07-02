166c74f5ffe057a50abf48feebd37905c85bb355
Rafael Matoso <rafaelmatosog@gmail.com>
2021-07-02 18:33:37 -0300

    Adicionando um Custom Marker ao mapa

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/dist/client/router'
 import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet'
+import L from 'leaflet'
 
 import * as S from './styles'
 
@@ -34,6 +35,13 @@ const CustomTileLayer = () => {
     />
   )
 }
+
+const markerIcon = new L.Icon({
+  iconUrl: 'img/icon-marker.png',
+  iconSize: [40, 40],
+  iconAnchor: [20, 20],
+  popupAnchor: [0, -40]
+})
 
 const Map = ({ places }: MapProps) => {
   const router = useRouter()
@@ -80,6 +88,7 @@ const Map = ({ places }: MapProps) => {
                   router.push(`/place/${slug}`)
                 }
               }}
+              icon={markerIcon}
             />
           )
         })}
